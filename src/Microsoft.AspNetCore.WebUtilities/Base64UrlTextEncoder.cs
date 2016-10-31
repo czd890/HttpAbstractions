86fc57ea9415fd811ba75b16b0d9657e9f432729
Kiran Challa <kichalla@microsoft.com>
2016-10-31 11:25:23 -0700

    Added doc comments

--- src/Microsoft.AspNetCore.WebUtilities/Base64UrlTextEncoder.cs
+++ src/Microsoft.AspNetCore.WebUtilities/Base64UrlTextEncoder.cs
@@ -7,11 +7,23 @@ namespace Microsoft.AspNetCore.WebUtilities
 {
     public static class Base64UrlTextEncoder
     {
+        /// <summary>
+        /// Encodes supplied data into Base64 and replaces any URL encodable characters into non-URL encodable
+        /// characters.
+        /// </summary>
+        /// <param name="data">Data to be encoded.</param>
+        /// <returns>Base64 encoded string modified with non-URL encodable characters</returns>
         public static string Encode(byte[] data)
         {
             return Convert.ToBase64String(data).TrimEnd('=').Replace('+', '-').Replace('/', '_');
         }
 
+        /// <summary>
+        /// Decodes supplied string by replacing the non-URL encodable characters with URL encodable characters and
+        /// then decodes the Base64 string.
+        /// </summary>
+        /// <param name="text">The string to be decoded.</param>
+        /// <returns>The decoded data.</returns>
         public static byte[] Decode(string text)
         {
             return Convert.FromBase64String(Pad(text.Replace('-', '+').Replace('_', '/')));
